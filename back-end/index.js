b403fd3391f416b115b218c07174ea2c1c1e06a5
Энх-Амар Болдбаатар <LPC240855@G401-CC0159.local>
2024-10-31 11:29:21 +0800

    comit

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,18 @@
-import express from "express";
+// import express from "express";
 
-const app = express();
-const port = 3535
+// const app = express();
+// const port = 3535;
 
-app.listen(port, ()=>{
-    console.log('server ajillaa')
-})
+// app.listen(port, () => {
+//   console.log("server ajillaa");
+// });
+
+var http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.write("hello world");
+    res.end("server ajillaa");
+    console.log("server ajillaa");
+  })
+  .listen(8800);
